d42aba1ce2df04dc79e6046dffe20d23b7e5a0d3
Iqra Fatima <iqrafatima1717@gmail.com>
2025-07-07 11:22:54 +0500

    Update quotes.ts

--- Nexium_IqraFatima_Assign1/quote_generator/src/data/quotes.ts
+++ Nexium_IqraFatima_Assign1/quote_generator/src/data/quotes.ts
@@ -12,6 +12,18 @@ export const quotes = [
     { topic: "inspiration", text: "Act as if what you do makes a difference. It does." },
     { topic: "motivation", text: "Your limitation—it's only your imagination." },
     { topic: "motivation", text: "Push yourself, because no one else is going to do it for you." },
-    { topic: "motivation", text: "Great things never come from comfort zones." }
+    { topic: "motivation", text: "Great things never come from comfort zones." },
+    {topic: "happiness", text: "Happiness is not something ready-made. It comes from your own actions." },
+    {topic: "happiness", text: "The purpose of our lives is to be happy." },
+    {topic: "happiness", text: "Happiness is a warm puppy." },
+    {topic: "wisdom", text: "The only true wisdom is in knowing you know nothing." },
+    {topic: "wisdom", text: "Wisdom begins in wonder." },
+    {topic: "wisdom", text: "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment." },
+    {topic: "courage", text: "Courage is not the absence of fear, but the triumph over it." },
+    {topic: "courage", text: "It takes courage to grow up and become who you really are." },
+    {topic: "courage", text: "Courage is grace under pressure." },          
+    {topic: "friendship", text: "Friendship is born at that moment when one person says to another, 'What! You too? I thought I was the only one.'" },
+    {topic: "friendship", text: "A friend is someone who knows all about you and still loves you." },
+    {topic: "friendship", text: "Friendship is the only cement that will ever hold the world together." }
     
 ];
