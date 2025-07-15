bfc51b34b3b9c6240b869d887a13c8abc43711e1
Iqra Fatima <iqrafatima1717@gmail.com>
2025-07-15 20:13:18 +0500

    Update layout.tsx

--- Nexium_IqraFatima_Assign2/ai_blog_summarizer/app/layout.tsx
+++ Nexium_IqraFatima_Assign2/ai_blog_summarizer/app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "AI Blog Summarizer",
+  description: "Transform lengthy blog posts into concise, intelligent summaries using advanced AI technology. Save time and get the key insights instantly.",
 };
 
 const geistSans = Geist({
